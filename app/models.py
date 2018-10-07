76fb238a43cd2c8c1b4fabd8f7201d8bf09f013f
朱元铮 <zhuyuanzheng@zhuyuanzhengdeMacBook-Pro.local>
2018-10-07 23:32:42 +0800

    add table followers

--- app/models.py
+++ app/models.py
@@ -5,6 +5,10 @@ from flask_login import UserMixin
 from app import login
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -46,7 +50,3 @@ class Post(db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
-followers = db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-)
